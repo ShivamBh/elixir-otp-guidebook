796ad402a844ad77766162afd7a531970ead79fc
ShivamBh <shivam.bhattacharjee94@gmail.com>
2023-05-29 22:43:33 +0530

    add simple worker

---
+++ metex_joe/lib/worker.ex
@@ -0,0 +1,45 @@
+defmodule MetexJoe.Worker do
+  # apikey = 57ec128e180737fdbe33736eff511b7b
+
+  def temperature_of(location) do
+    result = url_for(location) |> HTTPoison.get |> parse_response
+    case result do
+      {:ok, temp} ->
+        "#{location}:  #{temp}°C"
+      :error ->
+        "#{location} not found"
+    end
+  end
+
+  # defp get_lat_long(location) do
+  #   location = URI.encode(location)
+  #   ""
+  # end
+
+  defp url_for(location) do
+    location = URI.encode(location)
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey()}"
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body |> JSON.decode! |> compute_temp
+  end
+
+  defp parse_response(_) do
+    :error
+  end
+
+  defp compute_temp(json) do
+    try do
+      temp = (json["main"]["temp"] - 273.15 |> Float.round(1))
+      {:ok, temp}
+    rescue
+       _ -> :error
+    end
+  end
+
+  defp apikey do
+    "57ec128e180737fdbe33736eff511b7b"
+  end
+
+end
